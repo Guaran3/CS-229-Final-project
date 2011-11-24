f250a880ce99048ebcf9f0e28551036089e98333
Chris Pak <chris.j.pak@stanford.edu>
2011-11-24 11:24:31 -0700

    currently fixing kmeans, which sometimes has overlapping points, which causes a divide by zero error.

--- base/kmeans.py
+++ base/kmeans.py
@@ -12,16 +12,21 @@ class kmeans:
         self.numclusters = clusters
         self.data = data.rssvec
         self.clusters = random.sample(self.data, clusters)
-        
+        self.oldclusters = [[0 for i in range(100)] for j in self.clusters]
+        self.used = []
 
-    def iteration(self):
+    def iterate(self):
         newclusters = [[0 for i in range(100)] for j in self.clusters]
         count = [0 for j in self.clusters]
         for inpv in self.data:
             index = self.findClosest(inpv)
             newclusters[index] = addv(newclusters[index], inpv)
             count[index] += 1
+            print min(count),
         self.oldclusters = self.clusters
+        for i in range(len(self.clusters)):
+            if count[i] is 0:
+                print i
         self.clusters = map(lambda x, y: map(lambda z: z/float(y), x), newclusters, count)
 
     def findClosest(self, inpv):
@@ -32,7 +37,22 @@ class kmeans:
             if distances[i] < minv:
                 minv = distances[i]
                 minindex = i
+                if distances[i] is 0:
+                    self.used.append(i)
         return minindex
+    
+    def getDelta(self):
+        return max([max(map(lambda x,y: abs(x-y), vec1, vec2)) for vec1, vec2 in zip(self.clusters, self.oldclusters)])
+        
+    def getMeans(self):
+        iteration = 1
+        self.iterate()
+        while self.getDelta() > self.conv:
+            print 'Current Delta is: ' + str(self.getDelta())
+            print 'On Iteration: ' + str(iteration)
+            self.iterate()
+            iteration += 1
+        print 'Final Delta is: ' + str(self.getDelta())
 
 def addv(vec1, vec2):
     return map(lambda x,y: x+y, vec1, vec2)
