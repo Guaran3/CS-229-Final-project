fa6aa8e954a15415683280466655411a0fd40beb
Alexander Huang <zhizhen@stanford.edu>
2011-12-13 22:46:05 -0800

    dunno if i've changed anything

--- chooseMax.m
+++ chooseMax.m
@@ -10,18 +10,21 @@ for i=1:n
     end
 end
 
+scale_lat=111200*2;
+scale_lon = 88519*2;
 lat_min = min(yvals(:,1));
 lat_max = max(yvals(:,1));
 lon_min = min(yvals(:,2));
 lon_max = max(yvals(:,2));
-lat_l = int8((lat_max-lat_min)*111200)+1;
-lon_l = int8((lon_max-lon_min)*88519)+1;
+lat_l = int8((lat_max-lat_min)*scale_lat)+1;
+lon_l = int8((lon_max-lon_min)*scale_lon)+1;
 lat_coord=0;
 lon_coord=0;
-tau = 3000;
-threshold = .1;
 
-for index=1:1         % the index we're predicting for
+tau = 10000;
+threshold = .3;
+
+for index=1:n         % the index we're predicting for
     
     % Calculate the distance. I'll optimize this later.
     W = zeros(n,1);
@@ -36,17 +39,23 @@ for index=1:1         % the index we're predicting for
         if W(i)>threshold
             for j=1:lat_l
                 for k=1:lon_l
-                    lat_coord = lat_min+double(j)/111200;
-                    lon_coord = lon_min+double(k)/88519;
-                    rsqr = ((lat_coord-yvals(i,1))*111200)^2+((lon_coord-yvals(i,2))*88519)^2;
-                    sigma = yvals(i,3);
+                    lat_coord = lat_min+double(j)/scale_lat;
+                    lon_coord = lon_min+double(k)/scale_lon;
+                    rsqr = ((lat_coord-yvals(i,1))*scale_lat)^2+((lon_coord-yvals(i,2))*scale_lon)^2;
+                    sigma = yvals(i,3)/3;
                     P(j,k)=P(j,k)+W(i)*exp(-rsqr/(2*sigma^2))/(2*sigma^2);
                 end
             end
         end
     end
     [row,col] = find(P==max(P(:)));
-    outputs(index,:)=[row/111200+lat_min col/88519+lon_min];
+    outputs(index,:)=[row/scale_lat+lat_min col/scale_lon+lon_min];
     
 end
-scatter(outputs(:,1), outputs(:,2), 'r')
+
+n_outputs = outputs;
+for i = 1:n
+    n_outputs(i,1) = n_outputs(i,1) +rand(1)*.0001;
+    n_outputs(i,2) = n_outputs(i,2) +rand(1)*.0001;
+end
+scatter(n_outputs(:,1), n_outputs(:,2))
